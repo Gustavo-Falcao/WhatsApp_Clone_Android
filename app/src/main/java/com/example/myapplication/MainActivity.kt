c9aac2e2a40e451552b62be2e284dcedc969c628
Gustavo <duartegusfalcao@gmail.com>
2026-09-02 21:17:02 -0300

    Finalizado tela de contatos.

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -11,36 +11,38 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
-import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.modifier.modifierLocalConsumer
+import androidx.compose.ui.text.style.LineHeightStyle
 import com.example.myapplication.ui.theme.GrayBox
 import com.example.myapplication.ui.theme.GrayTextForeGround
+import com.example.myapplication.ui.theme.GreenBackGroundNavBar
+import com.example.myapplication.ui.theme.GreenPrimary
 import com.example.myapplication.ui.theme.GreenSecondary
+import com.example.myapplication.ui.theme.GreenShadow
 import com.example.myapplication.ui.theme.MainBackGroundColor
 import com.example.myapplication.ui.theme.MyApplicationTheme
 import org.w3c.dom.Text
@@ -89,10 +91,8 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 
     Column (
         modifier = modifier
-            .fillMaxSize()
-            .background(color = MainBackGroundColor),
-
-        verticalArrangement = Arrangement.spacedBy(20.dp)
+            .fillMaxWidth()
+            .background(color = MainBackGroundColor)
     ) {
         Column(
             modifier = Modifier
@@ -172,18 +172,136 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
             }
         }
 
-        Column() {
-            //fazer for chamando contato element
+        Column(modifier = Modifier
+            .padding(top = 10.dp)
+            .weight(1f)
+            .verticalScroll(rememberScrollState()),
+            verticalArrangement = Arrangement.spacedBy(15.dp),
 
-            ContatoElement()
+        ) {
+            //fazer for chamando contato element
+            //passar parametro indicando se o contatoElement é o último da lista
+
+            for (i in 1 .. 15) {
+                val isNotificationViewd = i % 2 == 0
+                val showSeparatorLine = i != 15
+                ContatoElement(isNotificationViewd, showSeparatorLine)
+            }
+
+
         }
-        Row() { }
+        Box() {
+            Icon(
+                painter = painterResource(id = R.drawable.chat_add),
+                contentDescription = "Chat icon",
+                tint = Color.Black,
+                modifier = Modifier
+                    .align(Alignment.TopEnd)
+                    .offset(
+                        x = (-10).dp,
+                        y = (-70).dp
+                    )
+                    .clip(RoundedCornerShape(20.dp))
+                    .background(color = GreenPrimary)
+                    .padding(18.dp)
+                    .size(24.dp)
+            )
+            MenuBar()
+        }
     }
 }
 
 
 @Composable
-fun ContatoElement() {
+fun MenuBar() {
+
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(color = GreenBackGroundNavBar)
+            .padding(15.dp),
+        horizontalArrangement = Arrangement.SpaceAround,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.spacedBy(5.dp)
+        ) {
+            Icon(
+                painter = painterResource(id = R.drawable.chat_icon),
+                contentDescription = "Chat icon",
+                tint = GreenPrimary,
+                modifier = Modifier
+                    .clip(RoundedCornerShape(20.dp))
+                    .background(color = GreenShadow)
+                    .padding(18.dp)
+                    .size(28.dp)
+            )
+            Text(
+                text = "Chats",
+                color = Color.White
+            )
+        }
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.spacedBy(5.dp)
+        ) {
+            Icon(
+                painter = painterResource(id = R.drawable.phone_icon),
+                contentDescription = "Phone icon",
+                tint = Color.White,
+                modifier = Modifier
+                    .clip(RoundedCornerShape(20.dp))
+                    .padding(18.dp)
+                    .size(28.dp)
+            )
+            Text(
+                text = "Calls",
+                color = Color.White
+            )
+        }
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.spacedBy(5.dp)
+        ) {
+            Icon(
+                painter = painterResource(id = R.drawable.groups_icon),
+                contentDescription = "Groups icon",
+                tint = Color.White,
+                modifier = Modifier
+                    .clip(RoundedCornerShape(20.dp))
+                    .padding(18.dp)
+                    .size(30.dp)
+            )
+            Text(
+                text = "Communities",
+                color = Color.White
+            )
+        }
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.spacedBy(5.dp)
+        ) {
+            Icon(
+                painter = painterResource(id = R.drawable.account_circle_icon),
+                contentDescription = "Account icon",
+                tint = Color.White,
+                modifier = Modifier
+                    .clip(RoundedCornerShape(20.dp))
+                    .padding(18.dp)
+                    .size(28.dp)
+            )
+            Text(
+                text = "You",
+                color = Color.White
+            )
+        }
+    }
+
+}
+
+@Composable
+fun ContatoElement(isNotificationViewd: Boolean, showSeparatorLine: Boolean) {
 
     Row(
         modifier = Modifier
@@ -191,24 +309,48 @@ fun ContatoElement() {
             .padding(horizontal = 20.dp)
     ) {
         Row(
-            modifier = Modifier.fillMaxWidth()) {
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.spacedBy(20.dp)
+        ) {
             Box(
                 modifier = Modifier
                     .clip(RoundedCornerShape(100.dp))
                     .background(color = GrayBox)
                     .padding(30.dp)
             ){}
-            Row(
+            Column(
                 modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(start = 5.dp),
-                horizontalArrangement = Arrangement.SpaceBetween
+                    .fillMaxWidth(),
+                verticalArrangement = Arrangement.spacedBy(15.dp)
             ) {
-                Column() {
-                    Text(text = "Contato", color = Color.White)
-                    Text(text = "Last message", color = GrayTextForeGround)
+                Row(
+                    modifier = Modifier
+                    .fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    verticalAlignment = Alignment.CenterVertically) {
+                    Column() {
+                        Text(text = "Contato", color = Color.White)
+                        Text(text = "Last message", color = GrayTextForeGround)
+                    }
+                    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                        Text(text = "hh:mm", color = GrayTextForeGround)
+                        Icon(
+                            painter = painterResource(id = R.drawable.check_message_icon),
+                            contentDescription = "Check icon",
+                            tint = if (isNotificationViewd) GreenPrimary else GrayTextForeGround ,
+                            modifier = Modifier.size(30.dp)
+                        )
+                    }
                 }
-                Text(text = "hh:mm", color = Color.White)
+                // se parametro passado for true box não será criada
+                if(showSeparatorLine) {
+                    Box(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .background(color = GrayTextForeGround)
+                            .padding(vertical = 1.dp)
+                    ) {}
+                }
 
             }
 
